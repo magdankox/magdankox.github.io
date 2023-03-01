d8d6e019f79eb085b66bba10da2871305d673718
magdankox <85492176+magdankox@users.noreply.github.com>
2023-03-01 15:18:21 +0100

    Add files via upload

---
+++ kartka.js
@@ -0,0 +1,54 @@
+import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
+
+// initialize kaboom context
+kaboom();
+
+// add a piece of text at position (120, 80)
+
+loadSprite("Kartka","swieta.png")
+
+loadSound("muzyka","Gdy_sliczna_panna.mp3 ")
+
+add([
+    sprite("Kartka"),
+    pos(0,0)
+])
+loadSprite("slimak","slimak.png")
+
+const slimak = add([
+    sprite("slimak"),
+    pos(100,300)
+])
+
+add([
+    play("muzyka")
+])
+
+let lewo=true
+
+onUpdate(()=>{
+
+    if (slimak.pos.x<500 && lewo)
+
+    slimak.pos.x=slimak.pos.x+1
+
+    else if (!lewo && slimak.pos.x > 0)
+    
+    {
+        slimak.pos.x=slimak.pos.x-1
+    }
+
+    else if(!lewo && slimak.pos.x <= 0)
+
+    {
+        lewo = true
+    }
+
+    else
+
+    {
+        lewo = false
+    }
+
+
+})
